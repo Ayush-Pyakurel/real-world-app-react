9f7cbb1984bef350baa4d5c62af47645e09ca5a7
Ayush-Pyakurel <ayushpyakurel449@gmail.com>
2022-12-21 15:27:09 +0545

    Login and Signup Form style for the forms

--- src/Pages/SignUp/Signup.tsx
+++ src/Pages/SignUp/Signup.tsx
@@ -1,8 +1,29 @@
+import signupStyles from "./Signup.module.css";
+
+import { Link } from "react-router-dom";
+
 const Signup = () => {
   return (
-    <div>
-      <h1>Signup</h1>
-    </div>
+    <>
+      <div className={signupStyles["signup-container"]}>
+        <div className={signupStyles["form-title"]}>
+          <h1>Sign up</h1>
+          <Link to={"/login"}>
+            {" "}
+            <span>Have an account?</span>
+          </Link>
+        </div>
+        <form className={signupStyles["signup-form"]}>
+          <label htmlFor="username"></label>
+          <input type="text" placeholder="username" id="username" />
+          <label htmlFor="email"></label>
+          <input type="email" placeholder="Email" id="email" />
+          <label htmlFor="password"></label>
+          <input type="password" placeholder="password" id="password" />
+          <button className={signupStyles["signup-btn"]}>Sign in</button>
+        </form>
+      </div>
+    </>
   );
 };
 
--- src/Pages/login/Login.tsx
+++ src/Pages/login/Login.tsx
@@ -1,7 +1,30 @@
 import { FC, ReactElement } from "react";
 
+import { Link } from "react-router-dom";
+
+import loginStyles from "./Login.module.css";
+
 const Login: FC = (): ReactElement => {
-  return <form></form>;
+  return (
+    <>
+      <div className={loginStyles["login-container"]}>
+        <div className={loginStyles["form-title"]}>
+          <h1>Sign in</h1>
+          <Link to={"/signup"}>
+            {" "}
+            <span>Need an account?</span>
+          </Link>
+        </div>
+        <form className={loginStyles["login-form"]}>
+          <label htmlFor="email"></label>
+          <input type="email" placeholder="Email" id="email" />
+          <label htmlFor="password"></label>
+          <input type="password" placeholder="password" id="password" />
+          <button className={loginStyles["login-btn"]}>Sign in</button>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default Login;

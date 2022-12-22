2fcd8fb6eac663fde0b7eeb7b00da618cb1178ab
Ayush-Pyakurel <ayushpyakurel449@gmail.com>
2022-12-22 10:07:07 +0545

    Login and SignUp form validation Install formik and yup

--- src/Pages/SignUp/Signup.tsx
+++ src/Pages/SignUp/Signup.tsx
@@ -1,8 +1,33 @@
+//styles imports
 import signupStyles from "./Signup.module.css";
 
+//router imports
 import { Link } from "react-router-dom";
 
+//formik hook import
+import { useFormik } from "formik";
+
+//yup import
+import * as YUP from "yup";
+
 const Signup = () => {
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+    onSubmit: (value) => {
+      console.log("signup", value);
+    },
+    validationSchema: YUP.object({
+      email: YUP.string()
+        .required("Email is required!")
+        .email("Invalid email!"),
+      username: YUP.string().required("Username is required!"),
+      password: YUP.string().required("Password is required!"),
+    }),
+  });
   return (
     <>
       <div className={signupStyles["signup-container"]}>
@@ -13,14 +38,53 @@ const Signup = () => {
             <span>Have an account?</span>
           </Link>
         </div>
-        <form className={signupStyles["signup-form"]}>
+        <form
+          className={signupStyles["signup-form"]}
+          onSubmit={formik.handleSubmit}
+        >
           <label htmlFor="username"></label>
-          <input type="text" placeholder="username" id="username" />
+          <input
+            type="text"
+            placeholder="username"
+            id="username"
+            value={formik.values.username}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+          />
+          <div className={signupStyles.error}>
+            {formik.errors.username &&
+              formik.touched.username &&
+              formik.errors.username}
+          </div>
           <label htmlFor="email"></label>
-          <input type="email" placeholder="Email" id="email" />
+          <input
+            type="email"
+            placeholder="Email"
+            id="email"
+            value={formik.values.email}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+          />
+          <div className={signupStyles.error}>
+            {formik.errors.email && formik.touched.email && formik.errors.email}
+          </div>
           <label htmlFor="password"></label>
-          <input type="password" placeholder="password" id="password" />
-          <button className={signupStyles["signup-btn"]}>Sign in</button>
+          <input
+            type="password"
+            placeholder="password"
+            id="password"
+            value={formik.values.password}
+            onBlur={formik.handleBlur}
+            onChange={formik.handleChange}
+          />
+          <div className={signupStyles.error}>
+            {formik.errors.password &&
+              formik.touched.password &&
+              formik.errors.password}
+          </div>
+          <button className={signupStyles["signup-btn"]} type="submit">
+            Sign in
+          </button>
         </form>
       </div>
     </>
--- src/Pages/login/Login.tsx
+++ src/Pages/login/Login.tsx
@@ -1,28 +1,97 @@
 import { FC, ReactElement } from "react";
 
+//router elememts import
 import { Link } from "react-router-dom";
 
+//styles import
 import loginStyles from "./Login.module.css";
 
+//formik imports
+import { useFormik } from "formik";
+
+//yup imports
+import * as YUP from "yup";
+
 const Login: FC = (): ReactElement => {
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+
+  const onSubmit = (value: any) => {
+    console.log("onSubmit", value);
+  };
+
+  const validationSchema = YUP.object({
+    email: YUP.string().required("Email is required").email("Invalid email!"),
+    password: YUP.string().required("Password is required"),
+  });
+
+  const formik: any = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: (value) => {
+      console.log("onSubmit", value);
+    },
+    validationSchema: YUP.object({
+      email: YUP.string().required("Email is required").email("Invalid email!"),
+      password: YUP.string().required("Password is required"),
+    }),
+  });
+
   return (
     <>
-      <div className={loginStyles["login-container"]}>
-        <div className={loginStyles["form-title"]}>
-          <h1>Sign in</h1>
-          <Link to={"/signup"}>
-            {" "}
-            <span>Need an account?</span>
-          </Link>
+      <>
+        <div className={loginStyles["login-container"]}>
+          <>
+            <div className={loginStyles["form-title"]}>
+              <h1>Sign in</h1>
+              <Link to={"/signup"}>
+                {" "}
+                <span>Need an account?</span>
+              </Link>
+            </div>
+            <form
+              className={loginStyles["login-form"]}
+              onSubmit={formik.handleSubmit}
+            >
+              <label></label>
+              <input
+                type="email"
+                placeholder="Email"
+                id="email"
+                name="email"
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+                value={formik.values.email}
+              />
+              <div className={loginStyles.error}>
+                {formik.errors.email &&
+                  formik.touched.email &&
+                  formik.errors.email}
+              </div>
+              <label htmlFor="password"></label>
+              <input
+                type="password"
+                placeholder="password"
+                id="password"
+                name="password"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+              />
+              <div className={loginStyles.error}>
+                {formik.errors.password &&
+                  formik.touched.password &&
+                  formik.errors.password}
+              </div>
+              <button className={loginStyles["login-btn"]}>Sign in</button>
+            </form>
+          </>
         </div>
-        <form className={loginStyles["login-form"]}>
-          <label htmlFor="email"></label>
-          <input type="email" placeholder="Email" id="email" />
-          <label htmlFor="password"></label>
-          <input type="password" placeholder="password" id="password" />
-          <button className={loginStyles["login-btn"]}>Sign in</button>
-        </form>
-      </div>
+      </>
     </>
   );
 };

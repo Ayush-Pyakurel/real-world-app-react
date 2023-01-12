1ffcb0a1ba2ea9483299193c98c08caffcf02338
Ayush-Pyakurel <ayushpyakurel449@gmail.com>
2023-01-12 13:53:23 +0545

    Articles display on MyArticles tab style articles inside MyArticles tab

--- src/Component/My Articles/MyArticles.tsx
+++ src/Component/My Articles/MyArticles.tsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 import stylesMyArticle from "./MyArticles.module.css";
 import { useAuthContext } from "../../Hooks/useAuthContext";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 
 export interface Author {
   username: string;
@@ -55,9 +57,9 @@ const MyArticles = () => {
   }, []);
 
   return (
-    <div>
+    <section>
       {loading ? (
-        <h5>loading articles...</h5>
+        <h5 className={stylesMyArticle.loading}>loading articles...</h5>
       ) : (
         <article>
           {myArticles.map((myArticle: any, index: number) => {
@@ -66,19 +68,36 @@ const MyArticles = () => {
                 key={index}
                 className={stylesMyArticle["myArticle-container"]}
               >
-                <div className={stylesMyArticle["user-detail"]}>
+                <figure>
                   <img src={myArticle.author.image} alt="user-image" />
+                </figure>
+                <div className={stylesMyArticle['user-like-container']}>
+                <div className={stylesMyArticle["user-detail"]}>
                   <Link to={`/profile/${myArticle.author.username}`}>
                     {myArticle.author.username}
                   </Link>
-                  
+                  <small>{myArticle.createdAt.toString().slice(0, 10)}</small>
+                </div>
+                <div className={stylesMyArticle.like}>
+                  <span>
+                    <FontAwesomeIcon icon={faHeart} />{" "}
+                    {myArticle.favoritesCount}
+                  </span>
+                </div>
+                </div>
+                <div className={stylesMyArticle["article-body"]}>
+                  <h3 className={stylesMyArticle.title}>{myArticle.title}</h3>
+                  <span className={stylesMyArticle.description}>
+                    {myArticle.description}
+                  </span>
+                  <Link to={`article/${myArticle.slug}`}>Read More...</Link>
                 </div>
               </div>
             );
           })}
         </article>
       )}
-    </div>
+    </section>
   );
 };
 

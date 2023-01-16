eb90ddc86d5865f4c9b2cbc00828f44c963d7f27
Ayush Pyakurel <ayushpyakurel449@gmail.com>
2023-01-16 08:05:29 +0545

    Working on Article Page

--- src/Pages/Article/Article.tsx
+++ src/Pages/Article/Article.tsx
@@ -1,8 +1,8 @@
-import axios from "axios";
-import { ReactElement, FC, useEffect, useState } from "react";
+import axios from 'axios';
+import { ReactElement, FC, useEffect, useState } from 'react';
 
-import stylesArticle from "./Article.module.css";
-import { useParams } from "react-router-dom";
+import stylesArticle from './Article.module.css';
+import { useParams } from 'react-router-dom';
 
 //interface for article response
 export interface Author {
@@ -44,16 +44,22 @@ const Article: FC = (): ReactElement => {
   return (
     <main className={stylesArticle.banner}>
       <h2>{articles?.article.title}</h2>
-      <div className={stylesArticle["user-detail"]}>
+      <div className={stylesArticle['user-detail']}>
         <figure>
-          <img src={articles?.article?.author?.image} alt="user-image" />
+          <img src={articles?.article?.author?.image} alt='user-image' />
           <figcaption>
-            <span className={stylesArticle.username}>{articles?.article?.author?.username}</span>
+            <span className={stylesArticle.username}>
+              {articles?.article?.author?.username}
+            </span>
             <br />
-            <span className={stylesArticle.date}>{articles?.article?.createdAt.toString().slice(0, 10)}</span>
+            <span className={stylesArticle.date}>
+              {articles?.article?.createdAt.toString().slice(0, 10)}
+            </span>
           </figcaption>
         </figure>
-        <button className={stylesArticle['edit-btn']}>edit</button>
+        <button className={stylesArticle['edit-btn']}>
+          <span>Edit Article</span>
+        </button>
         <button className={stylesArticle['delete-btn']}>delete</button>
       </div>
     </main>

5537579a981dbf25dd94ee8ebe53335f643beb8e
Ayush-Pyakurel <ayushpyakurel449@gmail.com>
2023-01-17 11:00:22 +0545

    Style Article Page

--- src/Pages/Article/Article.tsx
+++ src/Pages/Article/Article.tsx
@@ -1,8 +1,12 @@
-import axios from 'axios';
-import { ReactElement, FC, useEffect, useState } from 'react';
+import axios from "axios";
+import { ReactElement, FC, useEffect, useState } from "react";
 
-import stylesArticle from './Article.module.css';
-import { useParams } from 'react-router-dom';
+import stylesArticle from "./Article.module.css";
+import { useParams } from "react-router-dom";
+
+//font-awesome icon
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPen, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 
 //interface for article response
 export interface Author {
@@ -42,27 +46,41 @@ const Article: FC = (): ReactElement => {
   }, []);
 
   return (
-    <main className={stylesArticle.banner}>
-      <h2>{articles?.article.title}</h2>
-      <div className={stylesArticle['user-detail']}>
-        <figure>
-          <img src={articles?.article?.author?.image} alt='user-image' />
-          <figcaption>
-            <span className={stylesArticle.username}>
-              {articles?.article?.author?.username}
+    <>
+      <main className={stylesArticle.banner}>
+        <h1>{articles?.article.title}</h1>
+        <div className={stylesArticle["user-detail"]}>
+          <figure>
+            <img src={articles?.article?.author?.image} alt="user-image" />
+            <figcaption>
+              <span className={stylesArticle.username}>
+                {articles?.article?.author?.username}
+              </span>
+              <br />
+              <span className={stylesArticle.date}>
+                {articles?.article?.createdAt.toString().slice(0, 10)}
+              </span>
+            </figcaption>
+          </figure>
+          <button className={stylesArticle["edit-btn"]}>
+            <span className={stylesArticle["edit-icon-text-wrapper"]}>
+              <FontAwesomeIcon icon={faPen} />
+              <span>Edit Article</span>
             </span>
-            <br />
-            <span className={stylesArticle.date}>
-              {articles?.article?.createdAt.toString().slice(0, 10)}
+          </button>
+          <button className={stylesArticle["delete-btn"]}>
+            <span className={stylesArticle["delete-icon-text-wrapper"]}>
+              <FontAwesomeIcon icon={faTrashCan} />
+              <span>Delete Article</span>
             </span>
-          </figcaption>
-        </figure>
-        <button className={stylesArticle['edit-btn']}>
-          <span>Edit Article</span>
-        </button>
-        <button className={stylesArticle['delete-btn']}>delete</button>
-      </div>
-    </main>
+          </button>
+        </div>
+      </main>
+      <article className={stylesArticle['article-body']}>
+          <p>{articles?.article?.body}</p>
+          <div/>
+      </article>
+    </>
   );
 };
 
